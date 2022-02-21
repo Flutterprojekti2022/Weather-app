af0a737c776b11fe0992d783d077ab7c9d7c8a02
enpassant <>
2022-02-21 06:58:29 +0200

    FutureBuilder removed from station.dart

--- lib/station.dart
+++ lib/station.dart
@@ -1,16 +1,15 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
 
-import 'station_card.dart';
+import 'package:weather_app/station_data.dart';
 
 class Station extends StatelessWidget {
   String title;
 
   Station(this.title);
 
-  var weather = fetchWeather();
+  //var weather = fetchWeather();
 
   @override
   Widget build(BuildContext context) {
@@ -18,95 +17,16 @@ class Station extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: FutureBuilder(
-        future: weather,
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.hasData) {
-            return Column(
-              children: [
-                Center(
-                  child: Image.asset('images/willab.gif'),
-                ),
-                StationCard(
-                  snapshot.data['tempnow'].toString(),
-                  'Temperature [C]',
-                  Icons.thermostat,
-                ),
-                StationCard(
-                  snapshot.data['precipitation1d'].toString(),
-                  'Rain in a day [mm]',
-                  Icons.water,
-                ),
-                StationCard(
-                  snapshot.data['windspeed'].toString(),
-                  'Wind [m/s]',
-                  Icons.air,
-                ),
-                StationCard(
-                  snapshot.data['winddir'].toString(),
-                  'Wind Direction',
-                  Icons.rotate_left,
-                ),
-                StationCard(
-                  snapshot.data['windchill'].toString(),
-                  'Feels Like [C]',
-                  Icons.accessibility,
-                ),
-                StationCard(
-                  snapshot.data['timestamp'],
-                  'Timestamp',
-                  Icons.access_time_filled,
-                ),
-                ElevatedButton.icon(
-                  onPressed: fetchWeather,
-                  icon: const Icon(
-                    Icons.downloading,
-                  ),
-                  label: const Text(
-                    'Reload',
-                  ),
-                )
-              ],
-            );
-          } else if (snapshot.hasError) {
-            return const Text(
-              'Error',
-            );
-          } else {
-            return const Text(
-              'Wait a moment',
-            );
-          }
-        },
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Center(
+              child: Image.asset('images/willab.gif'),
+            ),
+            StationData(),
+          ],
+        ),
       ),
     );
   }
-}
-
-fetchWeather() async {
-  var data = '''
-    {
-      "tempnow": -10.6,
-      "temphi": -3.8,
-      "templo": -12.9,
-      "dewpoint": -11.6,
-      "humidity": 93,
-      "airpressure": 986.3,
-      "windspeed": 1.0,
-      "windspeedmax": 2.2,
-      "winddir": 240,
-      "precipitation1d": 0.0,
-      "precipitation1h": 0.0,
-      "solarrad": 1113,
-      "windchill": -10.6,
-      "timestamp": "2022-02-20 19:14 EET"
-    }
-  ''';
-  return jsonDecode(data);
-}
-
-_fetchWeather() async {
-  final url = Uri.parse('http://weather.willab.fi/weather.json');
-  var response = await get(url);
-  return jsonDecode(response.body);
 }
