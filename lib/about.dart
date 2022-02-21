03524dc3dd5903ea438ad89a24275334395a88f4
enpassant <>
2022-02-21 13:45:14 +0200

    slider for about page

--- lib/about.dart
+++ lib/about.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
-class About extends StatelessWidget {
+class About extends StatefulWidget {
   const About({Key? key}) : super(key: key);
+
+  @override
+  State<About> createState() => _AboutState();
+}
+
+class _AboutState extends State<About> {
+  double sliderValue = 0;
+
+  setSliderValue(double newValue) {
+    setState(() {
+      sliderValue = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +22,25 @@ class About extends StatelessWidget {
       appBar: AppBar(
         title: const Text('About'),
       ),
-      body: null,
+      body: Row(
+        children: [
+          Expanded(
+            child: Slider(
+              min: 0,
+              max: 100,
+              divisions: 100,
+              onChanged: setSliderValue,
+              value: sliderValue,
+            ),
+          ),
+          Text(
+            sliderValue.round().toString(),
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
