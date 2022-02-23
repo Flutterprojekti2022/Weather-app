abe4473d728ba6ddb1364514e5cf53424500bffa
ziptyr <>
2022-02-23 10:20:29 +0200

    removed comment

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,6 @@ main() async {
 }
 
 class WeatherApp extends StatelessWidget {
-  //const WeatherApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
